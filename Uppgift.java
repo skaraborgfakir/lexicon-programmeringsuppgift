31106ddba3363074144e53da8d9aca79e7e555b9
Stefan Niskanen Skoglund <stefan.skoglund@agj.net>
2021-04-03 16:47:17 +0200

    fler funktioner aktiverade (inget innehåll)

--- Uppgift.java
+++ Uppgift.java
@@ -1,9 +1,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Uppgift {
-    BufferedReader reader;
+    // BufferedReader reader;
+    static Scanner tangentbordsläsare;
 
     //
     // 1
@@ -15,47 +17,51 @@ public class Uppgift {
     //
     // 2
     //
-    void InputNamn() {
+    private static void InputNamn() {
+	System.out.print("Mata in ett namn:");
+	String namn = tangentbordsläsare.nextLine();
     }
     //
     // 3
     //
-    void BytFärg() {
+    private static void BytFärg() {
+	System.out.println( "BytFärg Hello world");
     }
     //
     // 4
     //
-    void DagensDatum() {
+    private static void DagensDatum() {
+	System.out.println( "DagensDatum Hello world");
     }
     //
     // 5
     //
-    void StörstTal() {
+    private static void StörstTal() {
     }
     //
     // 6
     //
-    void GissaEttNummer() {
+    private static void GissaEttNummer() {
     }
     //
     // 7
     //
-    void SkrivEnRadTillFil() {
+    private static void SkrivEnRadTillFil() {
     }
     //
     // 8
     //
-    void LäsEnRadFrånFil() {
+    private static void LäsEnRadFrånFil() {
     }
     //
     // 9
     //
-    void RäkningPåReellaTal() {
+    private static void RäkningPåReellaTal() {
     }
     //
     // 10
     //
-    void MultiplikationsTabell() {
+    private static void MultiplikationsTabell() {
 	//
 	// multiplikationstabell för talen 1-10
 	//
@@ -63,12 +69,12 @@ public class Uppgift {
     //
     // 11
     //
-    void TvåMatriser() {
+    private static void TvåMatriser() {
     }
     //
     // 12
     //
-    void ÄrEttOrdEnPalidrom() {
+    private static void ÄrEttOrdEnPalidrom() {
 	//
 	// ex ProtorP
 	//
@@ -76,7 +82,27 @@ public class Uppgift {
     //
     // 13
     //
-    void TalIntervall() {
+    private static void TalIntervall() {
+    }
+    //
+    // 14
+    //
+    private static void SorteringAvTalföljd() {
+	System.out.println( "SorteringAvTalföljd Hello world");
+
+    }
+    //
+    // 15
+    //
+    private static void SummeringAvTalföljd() {
+	System.out.println( "SummeringAvTalföljd Hello world");
+    }
+    //
+    // 16
+    //
+    private static void TvåKlasser() {
+	System.out.println( "TvåKlasser Hello world");
+
     }
 
     private static int VäljFunktion()
@@ -84,7 +110,11 @@ public class Uppgift {
 	String[] TillgängligaVal = {
 	    "Avsluta",
 	    "Hello world",
-	    "Mata in ett namn"
+	    "Mata in ett namn",
+	    "Byt konsollens färg",
+	    "Dagens datum",
+	    "Största Talet",
+
 	};
 
 	int ValLoop = 1;
@@ -96,27 +126,79 @@ public class Uppgift {
 	    if ( ValLoop==0 )
 		Klart = true;
 	    ValLoop = ValLoop+1;
-	    if (ValLoop==3) {
+	    if (ValLoop==5) {
 		ValLoop=0;
 	    }
 	};
 
-	int valdFunktion = reader.nextInt();
+	int valdFunktion = tangentbordsläsare.nextInt();
+
+	return valdFunktion;
     }
 
     public static void main(String[] args)
 	throws IOException
     {
-	reader = new BufferedReader(new InputStreamReader(System.in));
-	// String name = reader.readLine();
+	// reader = new BufferedReader(new InputStreamReader(System.in));
+	tangentbordsläsare = new Scanner(System.in);
+
 	int ValdFunktion = 1;
 	while (ValdFunktion != 0)
 	    {
 		ValdFunktion = VäljFunktion();
+
+		switch(ValdFunktion)
+		    {
+		    case 1:
+			Helloworld();
+			break;
+		    case 2:
+			InputNamn();
+			break;
+		    case 3:
+			BytFärg();
+			break;
+		    case 4:
+			DagensDatum();
+			break;
+		    case 5:
+			StörstTal();
+			break;
+		    case 6:
+			GissaEttNummer();
+			break;
+		    case 7:
+			SkrivEnRadTillFil();
+			break;
+		    case 8:
+			LäsEnRadFrånFil();
+			break;
+		    case 9:
+			RäkningPåReellaTal();
+			break;
+		    case 10:
+			MultiplikationsTabell();
+			break;
+		    case 11:
+			TvåMatriser();
+			break;
+		    case 12:
+			ÄrEttOrdEnPalidrom();
+			break;
+		    case 13:
+			TalIntervall();
+			break;
+		    case 14:
+			SorteringAvTalföljd();
+			break;
+		    case 15:
+			SummeringAvTalföljd();
+			break;
+		    case 16:
+			TvåKlasser();
+			break;
+		    }
 	    };
-
-	//	Helloworld(); // System.out.println(name);
-
     }
 }
 
